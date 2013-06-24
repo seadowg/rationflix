faa63e0bed09547306ee198d9242f5c2fa522f86
Callum Stott <oetzi101@gmail.com>
2013-06-24 00:36:17 -0700

    Use ScalaTest instead of specs2 for testing :neckbeard:

--- test/ApplicationSpec.scala
+++ test/ApplicationSpec.scala
@@ -1,32 +1,30 @@
 package test
 
-import org.specs2.mutable._
+import org.scalatest.FunSpec
 
 import play.api.test._
 import play.api.test.Helpers._
 
-/**
- * Add your spec here.
- * You can mock out a whole application including requests, plugins etc.
- * For more information, consult the wiki.
- */
-class ApplicationSpec extends Specification {
+class ApplicationSpec extends FunSpec {
   
-  "Application" should {
+  describe("Application") {
     
-    "send 404 on a bad request" in {
+    it("sends 404 on a bad request") {
       running(FakeApplication()) {
-        route(FakeRequest(GET, "/boum")) must beNone        
+        assert(route(FakeRequest(GET, "/boum")).isEmpty)
       }
     }
-    
-    "welcome the user" in {
-      running(FakeApplication()) {
-        val home = route(FakeRequest(GET, "/")).get
-        
-        status(home) must equalTo(OK)
-        contentType(home) must beSome.which(_ == "text/html")
-        contentAsString(home) must contain ("Welcome to Rationflix")
+
+    describe("index action") {
+
+      it ("welcome the user") {
+        running(FakeApplication()) {
+          val home = route(FakeRequest(GET, "/")).get
+
+          assert(status(home) == OK)
+          assert(contentType(home).get == "text/html")
+          assert(contentAsString(home).contains("Welcome to Rationflix"))
+        }
       }
     }
   }
--- test/IntegrationSpec.scala
+++ test/IntegrationSpec.scala
@@ -1,25 +1,18 @@
 package test
 
-import org.specs2.mutable._
+import org.scalatest.FunSpec
 
 import play.api.test._
 import play.api.test.Helpers._
 
-/**
- * add your integration spec here.
- * An integration test will fire up a whole play application in a real (or headless) browser
- */
-class IntegrationSpec extends Specification {
+class IntegrationSpec extends FunSpec {
   
-  "Application" should {
-    
-    "work from within a browser" in {
+  describe("/index") {
+
+    it("has the correct welcome message") {
       running(TestServer(3333), HTMLUNIT) { browser =>
-
         browser.goTo("http://localhost:3333/")
-
-        browser.pageSource must contain("Welcome to Rationflix")
-       
+        assert(browser.pageSource.contains("Welcome to Rationflix"))
       }
     }
     
